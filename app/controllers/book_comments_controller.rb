dd67f5f480ed0172a9adc1a20d57ba58252a95ac
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-16 22:26:45 +0900

    [Add]books,commentsコントローラ修正　コメントフォーム,投稿機能追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,6 +2,7 @@ class BookCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_params)
+    comment.book_id = book.id
     comment.save
     redirect_to request.referer
   end
@@ -9,6 +10,7 @@ class BookCommentsController < ApplicationController
   def destroy
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.find_by(book_params)
+    comment.book_id = book.id
     comment.destroy
     redirect_to request.referer
   end
