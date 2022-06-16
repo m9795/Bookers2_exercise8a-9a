9533d578033777abe3d09a4291740f08be3971ee
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-16 15:45:23 +0900

    [Add]book_commentsコントローラ create,destroy追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,2 +1,22 @@
 class BookCommentsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_params)
+    comment.save
+    redirect_to request.referer
+  end
+  
+  def destroy
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.find_by(book_params)
+    comment.destroy
+    redirect_to request.referer
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book_comment).permit(:comment)
+  end
+  
 end
