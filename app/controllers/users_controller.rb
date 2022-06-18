c7849c438b13436c824f48f843e35fc5cca5b694
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-18 10:40:27 +0900

    [Add]フォローフォロワー一覧ページの表示(内容はまだ)ルーティング,relationshopsコントローラ,_infoビューの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,16 +30,6 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
-  
-  def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
-  end
-
-  def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
-  end
 
   private
 
