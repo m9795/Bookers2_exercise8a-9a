2d327b04fee60da300157a72c49da5df47017dbf
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-19 16:31:06 +0900

    [Add]検索機能実装　検索結果一覧表示ページとカラム検索が不十分

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,13 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
   def search
-    if params[:name_key]
-      @users = User.name('name LIKE?', "%#{params[:name_kye]}%")
+    @word = params[:word]
+    @range = params[:range]
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
     else
-      @users = User.all
+      @books = Book.looks(params[:search], params[:word])
     end
   end
 end
